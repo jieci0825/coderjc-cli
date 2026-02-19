1884036319ef6ed32aad627ddf4e081e8187394b
coderjc <coderjc@qq.com>
2026-02-19 16:46:29 +0800

    chore: 格式化tsdown.config.ts文件，统一缩进和样式

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: 'esm',
-  clean: true,
-  banner: { js: '#!/usr/bin/env node' },
+    entry: ['src/index.ts'],
+    format: 'esm',
+    clean: true,
+    banner: { js: '#!/usr/bin/env node' },
 })
